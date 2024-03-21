cb918972d0d81fa4947c0683ee7808348f8323e3
S_Earthz_8 <118380195+LuZernSUchart@users.noreply.github.com>
2024-03-22 00:46:51 +0700

    Add files via upload

---
+++ Lab1.1 .cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("กรอกจำนวนบรรทัดที่ต้องการ : ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Hello World\n");
+    }
+
+}
